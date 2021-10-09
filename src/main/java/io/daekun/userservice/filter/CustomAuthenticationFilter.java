188d8804f1c6cd24800c48e48165bf6537138db0
daekun0920 <dosemfb0920@gmail.com>
2021-10-09 18:04:59 +0900

    # Access Token and Refresh Token

--- src/main/java/io/daekun/userservice/filter/CustomAuthenticationFilter.java
+++ src/main/java/io/daekun/userservice/filter/CustomAuthenticationFilter.java
@@ -1,10 +1,14 @@
 package io.daekun.userservice.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -12,6 +16,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Slf4j
 public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
@@ -42,15 +48,42 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
 
     /**
      * This method will be called if authentication is successful.
+     *
      * @param request
      * @param response
      * @param chain
-     * @param authResult
+     * @param authentication
      * @throws IOException
      * @throws ServletException
      */
     @Override
-    protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        super.successfulAuthentication(request, response, chain, authResult);
+    protected void successfulAuthentication(HttpServletRequest request,
+                                            HttpServletResponse response,
+                                            FilterChain chain,
+                                            Authentication authentication) throws IOException, ServletException {
+        User user = (User) authentication.getPrincipal(); // User that's been successfully authenticated.
+
+        Algorithm algorithm = Algorithm.HMAC256("secret".getBytes()); // "secret" should be replaced with something more secured in production.
+
+        String accessToken = JWT.create()
+                .withSubject(user.getUsername()) // something unique about the user
+                .withExpiresAt(new Date(System.currentTimeMillis() + 10 * 60 * 1000)) // 10 min
+                .withIssuer(request.getRequestURL().toString())
+                .withClaim(
+                        "roles",
+                        user.getAuthorities()
+                                .stream()
+                                .map(GrantedAuthority::getAuthority).collect(Collectors.toList())
+                )
+                .sign(algorithm);
+
+        String refreshToken = JWT.create()
+                .withSubject(user.getUsername()) // something unique about the user
+                .withExpiresAt(new Date(System.currentTimeMillis() + 30 * 60 * 1000)) // 30 min
+                .withIssuer(request.getRequestURL().toString())
+                .sign(algorithm);
+
+        response.setHeader("access_token", accessToken);
+        response.setHeader("refresh_token", refreshToken);
     }
 }
